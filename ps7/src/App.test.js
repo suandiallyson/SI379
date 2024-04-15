bcdd23dc5401d686f56a100e398d942deace0589
suandiallyson <112599106+suandiallyson@users.noreply.github.com>
2024-04-14 23:24:49 -0400

    Delete ps7 directory

--- ps7/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import LemonTimerApp from './App';
-
-test('renders learn react link', () => {
-  render(<LemonTimerApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
