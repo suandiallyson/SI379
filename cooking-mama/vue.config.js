91b3fc17e7857e6b79a2bc97cb9a1e0c4e1bcfa0
suandiallyson <suandial@umich.edu>
2024-04-27 03:43:02 -0400

    fix

--- cooking-mama/vue.config.js
+++ cooking-mama/vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
-    process.env.NODE_ENV === "production" ? "/SI379/cooking-mama/" : "/",
+    process.env.NODE_ENV === "production" ? "/SI379/cooking-mama/dist/" : "/",
 });
